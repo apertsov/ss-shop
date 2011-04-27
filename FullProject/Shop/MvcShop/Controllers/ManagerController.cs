0357d003811322a9a58ea3a442c1bbca96e0dbbf
petolegmuh@gmail.com <petolegmuh@gmail.com@37da1745-e476-ee7f-8808-3e4f477f1f8e>
2011-04-27 18:47:37 +0000

    manager role add remove

---
+++ FullProject/Shop/MvcShop/Controllers/ManagerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MvcShop.Controllers
+{
+    public class ManagerController : Controller
+    {
+        //
+        // GET: /Manager1/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Role()
+        {
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Role(string Name, string a, string delete, string role)
+        {
+            if(Name!=null)
+            if(!Roles.RoleExists(Name))
+            Roles.CreateRole(Name); 
+            if(delete!=null)
+                Roles.DeleteRole(role);
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Delete(string role, string delete)
+        {
+            if (delete != null)
+            {
+                string[] user = Roles.GetUsersInRole(role);
+                if (user.Length!=0)
+                   Roles.RemoveUsersFromRole(Roles.GetUsersInRole(role),role);
+                Roles.DeleteRole(role);
+            }
+            return RedirectToAction("Role","Manager");
+        }
+
+        public ActionResult UserRole()
+        {
+            return View();
+        }
+    }
+}
---
+++ FullProject/Shop/MvcShop/Views/Manager/Role.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Role
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+<h2>Role</h2>
+<%string[] str=Roles.GetAllRoles();%>
+<%for (int i = 0; i < str.Length; i++)
+{%>  
+    <%using (Html.BeginForm("Delete","Manager")){ %>
+      <!--<input type="hidden" value="<%=str[i]%>" name="role" />-->
+      <%=Html.Hidden("role",str[i]) %>
+      <%=str[i]%>
+      <input type="submit" value="X" name="delete" /> <br/>
+<%}%>
+      <br/>
+ <%} %>
+      Add new Role
+           <%using (Html.BeginForm()){ %>
+             Name Role<%=Html.TextBox("Name"," ") %>
+             <input type="submit" value="Add" name="a"/>
+      <%} %>
+</asp:Content>
