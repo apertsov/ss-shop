c0e3198704702f339956211a4ed5aeb4ed4e6cc0
RomanOlexLytvyn@gmail.com <RomanOlexLytvyn@gmail.com@37da1745-e476-ee7f-8808-3e4f477f1f8e>
2011-05-12 11:05:08 +0000

    Касир і Менеджер

---
+++ FullProject/Shop/MvcShop/Controllers/CashierController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcShop.ServiceShop;
+using ShopModel.Entities;
+
+namespace MvcShop.Controllers
+{
+    public class CashierController : Controller
+    {
+        //
+        // GET: /Cashier/
+
+        public ActionResult Index()
+        {
+            ServiceShopClient ssc = new ServiceShopClient();
+            ViewData["ordersPrepared"] = ssc.LoadOrderByStatus(OrderStatus.Prepared);
+            ViewData["ordersTaken"] = ssc.LoadOrderByStatus(OrderStatus.Taken);
+            ssc.Close();
+            
+            return View();
+        }
+
+        public ActionResult Confirm(Order order, string confirm, string cansel, string send)
+        {
+            if (confirm != null)
+            {
+                ServiceShopClient ssc = new ServiceShopClient();
+                order = ssc.LoadOrder(order.Id);
+                order.OrderStatus = OrderStatus.Processed;
+                ssc.UpdateOrder(order);
+                ssc.Close();
+            }
+            if (cansel != null)
+            {
+                ServiceShopClient ssc = new ServiceShopClient();
+                order = ssc.LoadOrder(order.Id);
+                order.OrderStatus = OrderStatus.Canseled;
+                ssc.UpdateOrder(order);
+                ssc.Close();
+            }
+
+            if (send != null)
+            {
+                ServiceShopClient ssc = new ServiceShopClient();
+                order = ssc.LoadOrder(order.Id);
+                order.OrderStatus = OrderStatus.End;
+                ssc.UpdateOrder(order);
+                ssc.Close();
+            }
+            return RedirectToAction("Index", "Cashier");
+        }
+    }
+}
